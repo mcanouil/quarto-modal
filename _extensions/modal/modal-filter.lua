98de851e8c9b4cb7606d9959a6d8c3b7ee2142c3
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2026-08-25 14:23:10 +0200

    refactor: sync shared Lua modules
    The shared modules follow the upstream copies again, and every LuaDoc
    module name is now double quoted.

--- _extensions/modal/modal-filter.lua
+++ _extensions/modal/modal-filter.lua
@@ -1,4 +1,4 @@
---- @module modal-filter
+--- @module "modal-filter"
 --- @license MIT
 --- @copyright 2026 Mickaël Canouil
 --- @author Mickaël Canouil
--- _extensions/modal/modal-shortcode.lua
+++ _extensions/modal/modal-shortcode.lua
@@ -1,4 +1,4 @@
---- @module modal-shortcode
+--- @module "modal-shortcode"
 --- @license MIT
 --- @copyright 2026 Mickaël Canouil
 --- @author Mickaël Canouil
